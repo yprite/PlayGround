27ec281c769b3355694763338da60ed92454fd3c
yprite <yprite@jjssm.org>
2019-02-15 16:45:35 +0900

    [Coming soon] Optimization size on index page

--- comingsoon/static/js/functions.js
+++ comingsoon/static/js/functions.js
@@ -55,6 +55,17 @@ jQuery(document).ready(function($) {
 
 	/* Time Countdown 
 	-------------------------------------------------------------------*/
+	function dateDiff(_date1, _date2) {
+		var diffDate_1 = _date1 instanceof Date ? _date1 : new Date(_date1);
+		var diffDate_2 = _date2 instanceof Date ? _date2 : new Date(_date2);
+
+		diffDate_1 = new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
+		diffDate_2 = new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+		var diff = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
+		diff = Math.ceil(diff / (1000 * 3600 * 24));
+		return diff;
+	}
+	 
 	$('#time_countdown').countDown({
         
         // targetDate: {
@@ -68,16 +79,15 @@ jQuery(document).ready(function($) {
         // omitWeeks: true
 
          targetOffset: {
-            'day':      0,
+            'day':      dateDiff('2019-06-01', new Date()),
             'month':    0,
-            'year':     1,
+            'year':     0,
             'hour':     0,
             'min':      0,
             'sec':      3
-		},
-		omitWeeks: true
-
-	    });
+	 },
+	omitWeeks: true
+	});
 
 
 
